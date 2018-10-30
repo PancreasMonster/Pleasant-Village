026df7e938c37f29f3188d14830066b35bd895cb
PancreasMonster <ryanflood444@gmail.com>
2018-10-30 11:49:40 +0000

    Movement Added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -16,8 +16,14 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.A)) {
-			rb.AddForce (Vector2.left * speed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (Vector3.left * speed * Time.deltaTime);
 		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Vector3.right * speed * Time.deltaTime);
+		}
+
+
 	}
 }
