b3e55a8d43edaeb45d0ce6a596fa76d505e0abfe
PancreasMonster <ryanflood444@gmail.com>
2018-10-30 11:38:04 +0000

    Movement Script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+
+	Rigidbody2D rb; 
+	[Range (0,20)]
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			rb.AddForce (Vector2.left * speed * Time.deltaTime);
+		}
+	}
+}
