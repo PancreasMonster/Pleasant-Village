73cd85cee9bcf4ab468d6375e8fcb8ee9ff1a01e
PancreasMonster <ryanflood444@gmail.com>
2018-10-30 17:36:27 +0000

    Asset Assignment

--- Assets/Scripts/BlockAssigner.cs
+++ Assets/Scripts/BlockAssigner.cs
@@ -8,20 +8,27 @@ public class BlockAssigner : MonoBehaviour {
 
 	public string block; 
 	public Flowchart flow;
+	public Sprite sprite1, sprite2;
+	SpriteRenderer sr;
+
 	// Use this for initialization
 	void Start () {
-		
+		sr = GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (flow.GetIntegerVariable ("ShopInt") > 1 && flow.GetIntegerVariable ("ShopInt") < 4) {
+			sr.sprite = sprite1;
+		} else if (flow.GetIntegerVariable ("ShopInt") == 4) {
+			sr.sprite = sprite2;
+		}
 	}
 
 	void OnTriggerStay2D(Collider2D col) {
 
 
-		if (Input.GetMouseButtonDown (0)){
+		if (Input.GetKeyDown (KeyCode.E)){
 			flow.ExecuteBlock(block);
 			Debug.Log ("Test");
 		}
--- Assets/Scripts/ShopApp.cs
+++ Assets/Scripts/ShopApp.cs
@@ -7,10 +7,14 @@ public class ShopApp : MonoBehaviour {
 
 	public Flowchart flow;
 	public int shopInt = 1;
+	public BoxCollider2D self, other; 
+	public AudioSource aud; 
+	public Animator anim;
+	public Movement move;
 
 	// Use this for initialization
 	void Start () {
-
+		self = GetComponent<BoxCollider2D> ();
 	}
 
 	// Update is called once per frame
@@ -20,5 +24,18 @@ public class ShopApp : MonoBehaviour {
 
 	void OnTriggerEnter2D () {
 		flow.SetIntegerVariable ("ShopInt", shopInt); 
+		shopInt += 2; 
+		self.enabled = false; 
+		other.enabled = true; 
+		aud.pitch -= .2f;
+		StartCoroutine (FadeScene ());
+	}
+
+	IEnumerator FadeScene() {
+		anim.SetBool ("Fade", true);
+		move.enabled = false; 
+		yield return new WaitForSeconds (1f);
+		anim.SetBool ("Fade", false);
+		move.enabled = true; 
 	}
 }
